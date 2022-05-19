1b754afff4c6347f5997e5062c29ba87bce98a08
Valentyn-Dominskyi <valentyn2202@gmail.com>
2022-05-19 10:26:50 +0300

    ✅: add CircularLinkedList Tests

---
+++ SML/SML.Tests/CircularLinkedListTests/CircularLinkedListNodeTests.cs
@@ -0,0 +1,48 @@
+﻿using SML.CircularLinkedList;
+using Xunit;
+
+namespace SML.Tests.CircularLinkedListTests;
+
+public class CircularLinkedListNodeTests
+{
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(1)]
+    [InlineData(int.MinValue)]
+    [InlineData(int.MaxValue)]
+    public void Constructor_Int_ReturnsCorrectValues(int expected)
+    {
+        // Arrange
+        var node = new CircularLinkedListNode<int>(expected);
+
+        // Act
+        var actualData = node.Data;
+        var actualNext = node.Next;
+
+        // Assert
+        Assert.Equal(expected, actualData);
+        Assert.Null(actualNext);
+    }
+
+    [Theory]
+    [InlineData("Паляниця")]
+    [InlineData("Русский военный корабль")]
+    [InlineData("European Union")]
+    [InlineData("汉字 and 漢字")]
+    [InlineData("الْعَرَبِيَّة")]
+    [InlineData("👾🤓😎🥸🤩🥳")]
+    public void Constructor_String_ReturnsCorrectValues(string expected)
+    {
+        // Arrange
+        var node = new CircularLinkedListNode<string>(expected);
+
+        // Act
+        var actualData = node.Data;
+        var actualNext = node.Next;
+
+        // Assert
+        Assert.Equal(expected, actualData);
+        Assert.Null(actualNext);
+    }
+}
