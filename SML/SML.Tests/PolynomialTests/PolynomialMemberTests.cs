e2ca2a1a72e10ab07ae618abdd1def8d86d982c5
Valentyn-Dominskyi <valentyn2202@gmail.com>
2022-06-04 17:24:19 +0300

    🎉: start Polynomials tests

---
+++ SML/SML.Tests/PolynomialTests/PolynomialMemberTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace SML.Tests.PolynomialTests;
+
+public class PolynomialMemberTests
+{
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(1)]
+    [InlineData(int.MinValue)]
+    [InlineData(int.MaxValue)]
+    public void Constructor_Int_ReturnsCorrectValues(int expected)
+    {
+        // Arrange
+        var node = new CircularLinkedListNode<int>(expected);
+
+        // Act
+        var actualData = node.Data;
+        var actualNext = node.Next;
+
+        // Assert
+        Assert.Equal(expected, actualData);
+        Assert.Null(actualNext);
+    }
+}
