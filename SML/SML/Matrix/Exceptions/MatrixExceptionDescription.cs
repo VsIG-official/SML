a3782188871df2f7c85288cf585e8fcf8481bf83
Valentyn-Dominskyi <valentyn2202@gmail.com>
2022-06-08 20:25:01 +0300

    ♻️: add better Matrices

---
+++ SML/SML/Matrix/Exceptions/MatrixExceptionDescription.cs
@@ -0,0 +1,13 @@
+﻿namespace SML.Matrices.Exceptions;
+
+public static class MatrixExceptionDescription
+{
+    public static string MatricesDifferentDimensionsExceptionMessage =
+        "Matrices should have same dimensions";
+    public static string NullMatrixExceptionMessage =
+        "Matrix shouldn't be null";
+    public static string RowsLessThanZeroExceptionMessage =
+        "Rows should be more than 0";
+    public static string ColumnsLessThanZeroExceptionMessage =
+        "Columns should be more than 0";
+}
